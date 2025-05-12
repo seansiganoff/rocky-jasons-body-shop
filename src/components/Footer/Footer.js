d42e82186e3f7cb552078ad96e8f1db3402c35a5
Sean Siganoff <nationwideclassics@yahoo.com>
2025-05-11 23:10:22 -0400

    fixed phone number

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -15,7 +15,7 @@ const Footer = () => {
                             {/* <a href="mailto:mobiledentandscratchrepair@yahoo.com"><img className="my-icons" src={require("../../images/email-icon.webp")} alt='Email icon'/></a> */}
                         </div>
                         <div className="desktop-footer">
-                        <p>CALL OR TEXT:  <b>214-556-8482</b></p>
+                        <p>CALL OR TEXT:  <b><a href="sms:214-556-8482">214-556-8482</a></b></p>
                         </div>
                     </div>
                 </div>
